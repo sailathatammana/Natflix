b1903c313dc3cb4cce5e52ad79aea59ba8eabade
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2022-10-11 22:21:20 +0200

    Encrypted password

--- src/main/java/com/novare/netflax/HelloInterface.java
+++ src/main/java/com/novare/netflax/HelloInterface.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HelloInterface extends JpaRepository<User, Long> {
 
-User findByEmail(String email);
+    User findByEmail(String email);
 }
--- src/main/java/com/novare/netflax/User.java
+++ src/main/java/com/novare/netflax/User.java
@@ -3,7 +3,6 @@ package com.novare.netflax;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
@@ -12,7 +11,7 @@ import javax.validation.constraints.NotEmpty;
 
 
 @Entity
-@Table(name="account")
+@Table(name = "account")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,19 +24,18 @@ public class User {
     private String email;
 
 
-    @Length(min = 5, max=100, message = "Password length most be between 5-100 characters")
+    @Length(min = 5, max = 100, message = "Password length most be between 5-100 characters")
     @Column(name = "password")
     private String password;
 
-    @Length(min = 3, max=100, message = "Name must be between 3-100 characters")
+    @Length(min = 3, max = 100, message = "Name must be between 3-100 characters")
     @Column(name = "name")
     private String name;
 
 
-
-
     // Hibernate needs a default constructor to function
-    public User() {}
+    public User() {
+    }
 
     public User(@Email(message = "Invalid email address! Please provide a valid email address") @NotEmpty(message = "Please provide an email address") String email, @Length(min = 5, max = 100, message = "Password length most be between 5-100 characters") String password, @Length(min = 3, max = 100, message = "Name must be between 3-100 characters") String name) {
         this.email = email;
