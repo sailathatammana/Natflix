d87fd97b250652c3a5c1b6882ce313eff1571e92
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2022-10-13 16:40:34 +0200

    Added endpoints for get,update and delete

--- src/main/java/com/novare/netflax/content/ContentController.java
+++ src/main/java/com/novare/netflax/content/ContentController.java
@@ -1,11 +1,13 @@
 package com.novare.netflax.content;
 
+import com.novare.netflax.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class ContentController {
@@ -18,8 +20,37 @@ public class ContentController {
     }
 
     @PostMapping("/content/create/")
-    public ResponseEntity<Content> createPost(@RequestBody Content content) {
+    public ResponseEntity<Content> createContent(@RequestBody Content content) {
         contentRepository.save(content);
         return ResponseEntity.status(HttpStatus.CREATED).body(content);
+    }
+
+    @GetMapping("/content")
+    public ResponseEntity<List<Content>> listAllContents() {
+        List<Content> contents = contentRepository.findAll();
+        return ResponseEntity.ok(contents);
+    }
+
+    @GetMapping(value = "/content", params = {"type_id"})
+    public ResponseEntity<List<Content>> getContentByType(@RequestParam int type_id) {
+        List<Content> contents = contentRepository.findAll().stream()
+                .filter((item) -> item.getType_id() == type_id)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(contents);
+    }
+
+    @PutMapping("/content/update/{id}")
+    public ResponseEntity<Content> updateContent(@PathVariable Long id, @RequestBody Content updatedContent) {
+        contentRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+        updatedContent.setId(id);
+        Content content = contentRepository.save(updatedContent);
+        return ResponseEntity.ok(updatedContent);
+    }
+
+    @DeleteMapping("/content/delete/{id}")
+    public ResponseEntity<Content> deletePost(@PathVariable Long id) {
+        Content content = contentRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+        contentRepository.delete(content);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
