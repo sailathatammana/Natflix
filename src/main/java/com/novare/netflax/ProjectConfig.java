8b4f0e8c7bb36dc596c7b287a2689362de3aca55
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2022-10-26 10:55:59 +0200

    Added config file

---
+++ src/main/java/com/novare/netflax/ProjectConfig.java
@@ -0,0 +1,48 @@
+package com.novare.netflax;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Disable cors, csrf for Stateless service handling
+        http
+                .cors().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        // Whitelist
+        http
+                .authorizeRequests().antMatchers("/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest().authenticated();
+
+    }
+
+
+    @Bean
+    public AuthenticationManager customAuthenticationManager() throws Exception {
+        return authenticationManager();
+    }
+
+
+}
